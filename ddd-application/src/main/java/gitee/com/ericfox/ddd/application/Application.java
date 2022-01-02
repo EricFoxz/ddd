787f58bf875bf3f855f9d59927294aff9ba4e1ec
EricFox <841958335@qq.com>
2022-01-02 22:57:49 +0800

    init

---
+++ ddd-application/src/main/java/gitee/com/ericfox/ddd/application/Application.java
@@ -0,0 +1,11 @@
+package gitee.com.ericfox.ddd.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ ddd-infrastructure/src/main/java/gitee/com/ericfox/ddd/infrastructure/general/StrUtil.java
@@ -0,0 +1,4 @@
+package gitee.com.ericfox.ddd.infrastructure.general;
+
+public class StrUtil extends cn.hutool.core.util.StrUtil {
+}
