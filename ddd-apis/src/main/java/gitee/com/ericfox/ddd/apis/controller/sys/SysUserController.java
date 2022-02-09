082a379b850bd921a5a666bf0c4c477d1c5e5452
EricFoxz <841958335@qq.com>
2022-02-09 16:58:47 +0800

    默认不开放修改的api

--- ddd-apis/src/main/java/gitee/com/ericfox/ddd/apis/controller/sys/SysUserController.java
+++ ddd-apis/src/main/java/gitee/com/ericfox/ddd/apis/controller/sys/SysUserController.java
@@ -1,10 +1,32 @@
 package gitee.com.ericfox.ddd.apis.controller.sys;
 
 import gitee.com.ericfox.ddd.apis.controller.sys.base.SysUserControllerBase;
+import gitee.com.ericfox.ddd.domain.sys.model.sys_user.SysUserEntity;
+import gitee.com.ericfox.ddd.infrastructure.general.toolkit.api.ResBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/sys/user")
 public class SysUserController extends SysUserControllerBase {
+    @Override
+    @PutMapping("/create")
+    public ResponseEntity<?> create(@RequestBody SysUserEntity sysUser) {
+        return ResBuilder.noData().setStatus(HttpStatus.SERVICE_UNAVAILABLE).build();
+    }
+
+    @Override
+    @PatchMapping("/edit")
+    public ResponseEntity<?> edit(SysUserEntity sysUser) {
+        return ResBuilder.noData().setStatus(HttpStatus.SERVICE_UNAVAILABLE).build();
+    }
+
+    @Override
+    @DeleteMapping("/remove")
+    public ResponseEntity<?> remove(SysUserEntity sysUser) {
+        return ResBuilder.noData().setStatus(HttpStatus.SERVICE_UNAVAILABLE).build();
+    }
+
 }
