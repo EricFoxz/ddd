9f526b8775cd344af39bd6dcd0917aebcba7d065
EricFoxz <841958335@qq.com>
2022-02-28 22:22:29 +0800

    debug

--- ddd-starter-oauth2/src/main/java/gitee/com/ericfox/ddd/starter/oauth2/interfaces/StarterOauth2Service.java
+++ ddd-starter-oauth2/src/main/java/gitee/com/ericfox/ddd/starter/oauth2/interfaces/StarterOauth2Service.java
@@ -3,4 +3,5 @@ package gitee.com.ericfox.ddd.starter.oauth2.interfaces;
 import com.fujieid.jap.core.JapUserService;
 
 public interface StarterOauth2Service extends JapUserService {
+    //https://justauth.plus/quickstart/jap-oauth2/
 }
