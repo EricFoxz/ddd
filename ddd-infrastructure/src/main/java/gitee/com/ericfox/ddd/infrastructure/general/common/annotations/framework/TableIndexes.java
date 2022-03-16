c9f15e513d8c91cdaaa921dfd77b50b0eddb472f
EricFoxz <841958335@qq.com>
2022-03-16 13:30:15 +0800

    debug

---
+++ ddd-infrastructure/src/main/java/gitee/com/ericfox/ddd/infrastructure/general/common/annotations/framework/TableIndexes.java
@@ -0,0 +1,5 @@
+package gitee.com.ericfox.ddd.infrastructure.general.common.annotations.framework;
+
+public @interface TableIndexes {
+    TableIndex value();
+}
--- ddd-infrastructure/src/main/java/gitee/com/ericfox/ddd/infrastructure/persistent/po/sys/SysUser.java
+++ ddd-infrastructure/src/main/java/gitee/com/ericfox/ddd/infrastructure/persistent/po/sys/SysUser.java
@@ -2,10 +2,7 @@ package gitee.com.ericfox.ddd.infrastructure.persistent.po.sys;
 
 import gitee.com.ericfox.ddd.common.enums.strategy.RepoTypeStrategyEnum;
 import gitee.com.ericfox.ddd.common.interfaces.BasePo;
-import gitee.com.ericfox.ddd.infrastructure.general.common.annotations.framework.FieldComment;
-import gitee.com.ericfox.ddd.infrastructure.general.common.annotations.framework.FieldLength;
-import gitee.com.ericfox.ddd.infrastructure.general.common.annotations.framework.TableComment;
-import gitee.com.ericfox.ddd.infrastructure.general.common.annotations.framework.TableIndex;
+import gitee.com.ericfox.ddd.infrastructure.general.common.annotations.framework.*;
 import gitee.com.ericfox.ddd.infrastructure.general.common.annotations.service.RepoEnabledAnnotation;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +11,9 @@ import lombok.Setter;
  * 系统用户表
  */
 @TableComment("系统用户表")
-@TableIndex(name = "sys_user__username", column = "username")
+@TableIndexes(
+        @TableIndex(name = "sys_user__username", column = "username")
+)
 @Setter
 @Getter
 @RepoEnabledAnnotation(type = RepoTypeStrategyEnum.MY_SQL_REPO_STRATEGY)
