92793e5761df8688444a90d0481ca04c0847501a
EricFoxz <841958335@qq.com>
2022-07-10 23:55:39 +0800

    引入kotlinx-coroutines。测试协程实现（未通过）

--- ddd-common/src/test/java/CoroutinesTest.java
+++ ddd-common/src/test/java/CoroutinesTest.java
@@ -1,37 +1,51 @@
 import cn.hutool.http.HttpUtil;
 import gitee.com.ericfox.ddd.common.toolkit.coding.CollUtil;
 import gitee.com.ericfox.ddd.common.toolkit.coding.FileUtil;
-import kotlinx.coroutines.scheduling.CoroutineScheduler;
+import gitee.com.ericfox.ddd.common.toolkit.coding.ThreadUtil;
+import kotlin.Unit;
+import kotlinx.coroutines.Dispatchers;
+import kotlinx.coroutines.Job;
+import kotlinx.coroutines.JobSupport;
 import kotlinx.coroutines.scheduling.Task;
+import kotlinx.coroutines.test.TestCoroutineContext;
+import org.objenesis.instantiator.util.UnsafeUtils;
 
-import java.io.File;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReferenceArray;
+import java.util.concurrent.locks.LockSupport;
 
 public class CoroutinesTest {
     private static final AtomicInteger integer = new AtomicInteger(0);
 
     public static void main(String[] args) {
-        CoroutineScheduler coroutineScheduler = new CoroutineScheduler(50, 10000, 1000L * 600, "");
+        Job job = new JobSupport(true);
+        job.invokeOnCompletion((Throwable throwable) -> {
+            int i = integer.incrementAndGet();
+            List<String> list = CollUtil.newArrayList("https://www.github.com/", "https://www.baidu.com/", "https://blog.csdn.net/");
+            String s = HttpUtil.get(list.get(i % 3));
+            FileUtil.appendUtf8String(i + "", "F:/Desktop/test.txt");
+            return Unit.INSTANCE;
+        });
+        job.start();
+
+        LockSupport.parkUntil(3000);
+
+
+        FileUtil.appendUtf8String("开始\n", "F:/Desktop/test.txt");
         Runnable task = new Task() {
             public void run() {
-                List<String> list = CollUtil.newArrayList("https://www.github.com/", "https://www.baidu.com/", "https://blog.csdn.net/");
                 int i = integer.incrementAndGet();
-                String s = HttpUtil.get(list.get(i%3));
-                FileUtil.appendUtf8String(i+"", FileUtil.file("F:/Desktop/test.txt"));
-
+                List<String> list = CollUtil.newArrayList("https://www.github.com/", "https://www.baidu.com/", "https://blog.csdn.net/");
+                String s = HttpUtil.get(list.get(i % 3));
+                FileUtil.appendUtf8String(i + "", "F:/Desktop/test.txt");
             }
         };
 
-        AtomicReferenceArray<CoroutineScheduler.Worker> workers = coroutineScheduler.workers;
-
-        coroutineScheduler.execute(task);
-        coroutineScheduler.execute(task);
-        coroutineScheduler.execute(task);
-        coroutineScheduler.execute(task);
-        coroutineScheduler.execute(task);
-        coroutineScheduler.execute(task);
+        Dispatchers.getDefault().dispatchYield(new TestCoroutineContext("test"), task);
+        Dispatchers.getDefault().dispatchYield(new TestCoroutineContext("test"), task);
+        Dispatchers.getDefault().dispatchYield(new TestCoroutineContext("test"), task);
+        Dispatchers.getDefault().dispatchYield(new TestCoroutineContext("test"), task);
+        Dispatchers.getDefault().dispatchYield(new TestCoroutineContext("test"), task);
+        Dispatchers.getDefault().dispatchYield(new TestCoroutineContext("test"), task);
     }
 }
